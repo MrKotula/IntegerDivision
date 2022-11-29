201532462ba6fbaf67e4dff7176fda225bf4f876
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2022-11-29 11:32:35 +0100

    Changed method checkNullDivisorTest()

--- IntegerDivision/src/test/java/ua/foxminded/integerdivision/DivisionTest.java
+++ IntegerDivision/src/test/java/ua/foxminded/integerdivision/DivisionTest.java
@@ -7,11 +7,10 @@ import org.junit.jupiter.api.Test;
 
 class DivisionTest {
 
-	Division testObject = new Division();
+	private Division testObject = new Division();
 
 	@Test
 	void shouldMakeDivisionTest() {
-
 		String expected = "_14789|20\n" + " 140  |---\n" + " ---  |739\n" + "  _78\n" + "   60\n" + "   --\n"
 				+ "  _189\n" + "   180\n" + "   ---\n" + "     9\n";
 		assertEquals(expected, testObject.division(14789, 20));
@@ -19,11 +18,13 @@ class DivisionTest {
 
 	@Test
 	void checkNullDivisorTest() {
+		int dividend = 14789;
+		int divisor = 0;
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			testObject.division(14789, 0);
+		testObject.division(dividend, divisor);
 		});
 	}
-
+	
 	@Test
 	void checkDividendLessDivisorTest() {
 		assertEquals("0/5=0", testObject.division(0, 5));
